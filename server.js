591582bc752df3f495019214d7b3c81c61738b36
Axel Obscura <axosar@gmail.com>
2020-04-15 17:15:07 -0500

    setting nodemon

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const next = require('next');
 const express = require('express');
 
 const dev = process.env.NODE_ENV !== "production";
-
 const port = process.env.PORT || 3000;
-
 const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
+    server.get('*', (req, res) => {
+        return handler(req, res);
+    });
     server.listen(port, err => {
         if (err) throw err;
         console.log(`Listening on port ${port}`);
