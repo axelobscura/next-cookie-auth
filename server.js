a6b7aae244d1861a547890addaabf677e6e1ab16
Axel Obscura <axosar@gmail.com>
2020-04-16 10:46:03 -0500

    Adding axios to fetch api

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3001;
 const app = next({ dev });
 const handler = app.getRequestHandler();
+const axios = require('axios');
+
+const authenticate = async (email, password) => {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return data.find(user => {
+        if (user.email === email && user.website === password) {
+            return user;
+        }
+    })
+}
 
 app.prepare().then(() => {
     const server = express();
     server.use(express.json());
-    server.post('/api/login', (req, res) => {
+    server.post('/api/login', async (req, res) => {
         const { email, password } = req.body;
-        res.json({
-            email,
-            password,
-            success: true
-        })
+        const userData = await authenticate(email, password);
     })
     server.get('*', (req, res) => {
         return handler(req, res);
