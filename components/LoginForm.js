ef706503994e88d640e63b70dbdf94b523079156
Axel Obscura <axosar@gmail.com>
2020-04-17 08:21:54 -0500

    Handling login errors

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -5,7 +5,9 @@ import Router from 'next/router';
 class LoginForm extends React.Component {
     state = {
         email: 'Shanna@melissa.tv',
-        password: 'anastasia.net'
+        password: 'anastasia.net',
+        error: '',
+        isLoading: false
     };
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value });
@@ -13,13 +15,20 @@ class LoginForm extends React.Component {
     handleSubmit = event => {
         const { email, password } = this.state;
         event.preventDefault();
+        this.setState({ error: '', isLoading: true });
         console.log(this.state);
         loginUser(email, password).then(() => {
             Router.push("/profile");
-        });
+        })
+            .catch(this.showError);
     };
+    showError = err => {
+        console.log(err);
+        const error = err.response && err.response.data || err.message;
+        this.setState({ error, isLoading: false })
+    }
     render() {
-        const { email, password } = this.state;
+        const { email, password, error, isLoading } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
                 <div>
@@ -28,7 +37,11 @@ class LoginForm extends React.Component {
                 <div>
                     <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} />
                 </div>
-                <button type="submit">Submit</button>
+                {error && <div>{error}</div>}
+                <button disabled={isLoading} type="submit">
+                    {isLoading ? 'Sending' : 'Submit'}
+                </button>
+
             </form>
         )
     }
